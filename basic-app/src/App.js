b596b00db86a2d9e8097de1cd65d2c0555f8e9dd
Seema Thapa <tseema@insightworkshop.io>
2020-07-30 17:40:54 +0545

    [Add] added handle click event function

--- basic-app/src/App.js
+++ basic-app/src/App.js
@@ -6,15 +6,25 @@ function App() {
     // creating a new state name counter and setCounter function
     // This useState function is called a hook in the react world.
     const [counter, setCounter] = useState(5);
+    const handleClick = () => setCounter(counter + 1);
+    //() => setCounter(counter + counter) arrow function example
     return (
-        // Event Handling and using arrow function syntax
+        // Event Handling and using arrow function syntax, don't use long lines as it's hard to read
         <div>
             <h3>Count Example for hook:</h3>
             <p>Explanation: Here, a new state counter is used to pass input value.
                 useState function acts as a hook which will update the value (make it reactive)</p>
-            <button onClick={() => setCounter(counter + counter)}> {counter} </button>
+            <button onClick={handleClick}> {counter} </button>
+            <Display/>
         </div>
     );
+}
+
+//for display purpose (presentational one only) without any stateful hook
+function Display() {
+    return(
+        <div>....</div>
+    )
 }
 
 export default App;
